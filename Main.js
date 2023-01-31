de66d78b8c2fae9e7ca170272c68df449883edff
Mogbojuri oluwasegun oluwatobi <121747856+mogbojuri-s@users.noreply.github.com>
2023-01-31 21:52:49 +0100

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,35 @@
+ document.querySelector("index.html").style.background ='red'
+ 
+const Custom Name=document.getElementBy Id
+      ('Custom name');
+      const randomize= document.querySelector('.randomize');
+      const story=document.querySelector('.story');
+function random Value from Array(array){
+    const random= math.floor(math.random()array length)
+;
+return array [random];}
+
+RAW TEXT STRINGS
+ It was 94 Fahrenheit outside , so : insert x: went for a walk . when they got to :insert y:,they started in horror for a few moments,then:insert z: Bob saw the whole thin,but was not surprised-:insert x: weight 300 pounds ,and it was a hot day.
+ 
+ willy the Goblin
+ Big Daddy
+ father Christmas
+ the soup kitchen
+ Disney land
+ the white house
+ spontaneously combusted
+ melted into a puddle on the side walkturned into a puddleand crawled back
+ Event listener and partial function definition
+ randomize.addEventlistener('Click',result)
+ function result(){
+     if (customName.value!==''){
+         const name=customName.Value;
+     }
+     if(document.getElementById('Uk')checked){
+      const weight=Math.round(300);
+      const temperature=Marh.round(94)
+   ;  }
+ Story.textcontent=;
+ Story.style.visibility='visible'
+}
